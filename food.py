c3c1e881a212473371d60521b965b094cb954246
Gergely Banyai <banyai.gergely@gmail.com>
2025-03-21 14:47:08 +0100

    The food functionality is completed and outsourced to a seperate modul, however the code could be improved, especially the delete part.

---
+++ food.py
@@ -0,0 +1,23 @@
+import random
+from turtle import Turtle
+
+class Food:
+    def __init__(self):
+        self.food_object = Turtle(shape="turtle")
+        self.food_object.hideturtle()
+        self.food_object.penup()
+        self.food_object.color("blue")
+        self.new_food()
+
+
+    def new_food(self):
+        x_cor = random.randint(-250, 250)
+        y_cor = random.randint(-250, 250)
+        self.food_object.goto(x_cor, y_cor)
+        self.food_object.dot(15)
+
+
+
+
+
+
