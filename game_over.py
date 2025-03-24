b58463c5b247196370743b6caa4dd7784ebeb8a5
Gergely Banyai <banyai.gergely@gmail.com>
2025-03-24 09:21:51 +0100

    Snake module is completed, everything is outsourced to seperate classes, best score is written out on the screen as well.

--- game_over.py
+++ game_over.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 14, "normal")
 
-
+# Freeze game and write out Game Over, when snake dies
 class GameOver:
     def __init__(self):
         self.tim = Turtle()
--- score.py
+++ score.py
@@ -3,7 +3,8 @@ ALIGNMENT = "center"
 FONT = ("Arial", 14, "normal")
 
 class Score:
-    def __init__(self, height):
+    def __init__(self, height, width):
+        # current score
         self.score = -1
         self.tim = Turtle()
         self.tim.hideturtle()
@@ -13,14 +14,36 @@ class Score:
         self.tim.pendown()
         self.increase_score()
 
+        # best score
+        self.best = self.best_score()
+        self.tom = Turtle()
+        self.tom.hideturtle()
+        self.tom.color("white")
+        self.tom.penup()
+        self.tom.goto(-(width / 2 - 70), height / 2 - 30)
+        self.tom.pendown()
+        self.tom.write(f"Best Score: {self.best}", align=ALIGNMENT, font=FONT)
+
 
     def increase_score(self):
         self.tim.clear()
         self.score += 1
         self.tim.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
+    def best_score(self):
+        with open("best_score.txt", mode="r") as b_file:
+            best_score = b_file.read()
 
+        return int(best_score)
 
+    def overwrite_bestscore(self):
+        try:
+            if self.score > self.best_score():
+                with open("best_score.txt", mode="w") as file:
+                    file.write(f"{self.score}")
+        except FileNotFoundError:
+            with open("best_score.txt", mode="w") as file:
+                file.write(f"{self.score}")
 
 
 
